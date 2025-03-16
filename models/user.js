80fa7c3b43cc64d06b1d2000aa25f45a7d95b322
tsztin0217 <tsztin0217@gmail.com>
2025-03-16 18:10:22 +0800

    Define user and log schema

--- models/user.js
+++ models/user.js
@@ -1,5 +1,45 @@
 const mongoose = require('mongoose');
 
+const logSchema = new mongoose.Schema({
+  mood: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  intensity: {
+    type: Number,
+    min: 1,
+    max: 10,
+    required: true
+  },
+  nextSteps: {
+    type: String
+  },
+  takeaway: {
+    type: String
+  },
+  progress: {
+    type:
+      String,
+    enum: [
+      'No Action Needed',
+      'Not Started',
+      'In Progress',
+      'Completed'],
+    default:
+      'No Action Needed',
+    required: true
+  }
+});
+
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -9,6 +49,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  logs: [logSchema]
 });
 
 const User = mongoose.model('User', userSchema);
