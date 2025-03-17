b149ead777562ee9f8cc428abec997289717956c
tsztin0217 <tsztin0217@gmail.com>
2025-03-18 00:42:57 +0800

    Modify moods index route to create unique moods to be shown in index page

--- controllers/logs.js
+++ controllers/logs.js
@@ -3,8 +3,17 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
-router.get('/', (req, res) => {
-    res.render('moods/index.ejs');
+router.get('/', async(req, res) => {
+    const currentUser = await User.findById(req.session.user._id);
+    const logs = currentUser.logs || [];
+    const uniqueMoods = logs.reduce((acc, log)=> {
+        if (!acc.includes(log.mood)){
+            acc.push(log.mood);
+        }
+        return acc;
+    }, []);
+    console.log(uniqueMoods)
+    res.render('moods/index.ejs', {currentUser, uniqueMoods});
 }) 
 
 
@@ -12,4 +21,18 @@ router.get('/new', (req, res) => {
     res.render('moods/new.ejs');
 })
 
+
+router.post('/', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.logs.push(req.body);
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/moods`)
+    } catch (error) {
+        console.log(error);
+        res.redirect('/')
+    }
+ })
+
+ 
 module.exports = router;
